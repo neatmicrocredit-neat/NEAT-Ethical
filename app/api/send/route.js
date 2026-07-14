c218d351e5cce78a6d3fb193299735ae2ceabad2
ogofe <7thogofe@gmail.com>
2026-07-14 10:10:43 +0100

    update fixes

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -1,39 +1,73 @@
 import { CustomerEmailTemplate } from '@/components/email/customer-mail';
-// import { EmailTemplate } from '@/components/email/customer-mail';
+import { StaffEmailTemplate } from '@/components/email/staff-mail';
 import { Resend } from 'resend';
 
-// const resend = new Resend("re_MZZUHst9_J5hnDFQxPb1fSDz3YRhySuMC");
-const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+async function sendStaffMail({
+  first_name,
+  last_name,
+  email,
+  phone_number,
+  amount,
+  vehicle,
+  submittedOn,
+  referenceId,
+  ...props
+}){
+  const { data, error } = await resend.emails.send({
+    from: 'NEAT Ethical Investments <signups@neatmicrocredit.com.ng>',
+    to: ['7thogofe@gmail.com'],
+    subject: 'Welcome to NEAT Ethical Investments',
+    react: StaffEmailTemplate({
+      first_name: first_name,
+      last_name: last_name,
+      phone_number: phone_number,
+      email: email
+    }),
+  });
+
+  if (error) {
+    console.log("Error sending mail to staff:", error)
+    throw Error("Error sending mail to staff", {
+      cause: error
+    })
+  }
+}
+
+async function sendCustomerMail({ first_name, last_name, amount, ...props }) {
+  const { data, error } = await resend.emails.send({
+    from: 'NEAT Ethical Investments <signups@neatmicrocredit.com.ng>',
+    to: [email],
+    subject: 'Welcome to NEAT Ethical Investments',
+    react: CustomerEmailTemplate({
+      first_name,
+      last_name,
+      amount
+    }),
+  });
+  
+  if (error) {
+    console.log("Error sending mail to customer:", error);
+    throw Error("Error sending mail to customer", {
+      cause: error
+    })
+  }
+}
+
 
 export async function POST(request) {
   try {
 
-    const oldData =  await request.json();
+    const formData =  await request.json();
+    console.log('Sending mail with formData', formData);
 
-    console.log('Sending mail with oldData', oldData);
-
-
-    const { data, error } = await resend.emails.send({
-    //   from: 'NEAT Ethical Investments <joel.o@neatmicrocredit.com.ng>',
-      from: 'NEAT Ethical Investments <tjogofe@gmail.com>',
-      to: [oldData.email],
-      subject: 'Welcome to NEAT Ethical Investments',
-      react: CustomerEmailTemplate({
-        first_name: oldData.first_name,
-        last_name: oldData.last_name,
-        phone_number: oldData.phone_number,
-        email: oldData.email
-      }),
-    });
-
-    if (error) {
-        console.log("Error sending mail:", error)
-        return Response.json({ error }, { status: 500 });
-    }
+    await sendStaffMail(formData);
+    await sendCustomerMail(formData);
     
     return Response.json({data, oldData}, { status: 200 });
 } catch (error) {
-      console.log("Error with mailer:", error)
+    console.log("Error with mailer:", error);
     return Response.json({ error }, { status: 500 });
   }
 }
